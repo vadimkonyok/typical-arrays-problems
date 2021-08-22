7cdf610230c4c4eac0e6feafea5065c88d52e2a3
vadimkonyok <vadim.konyok@mail.ru>
2021-08-22 14:29:07 +0300

    feat: complete Typical Arrays Problems task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
 
 exports.min = function min (array) {
-  return 0;
+  if(!array || array.length === 0) {
+  return 0
+    }else{
+  return Math.min(...array);
+  }
 }
+  
+
 
 exports.max = function max (array) {
-  return 0;
-}
+    if(!array || array.length === 0) {
+      return 0
+    }else{
+      return Math.max(...array);
+    }
+  }
+
 
 exports.avg = function avg (array) {
-  return 0;
+    if(!array || array.length === 0) {
+      return 0
+    }else{
+    let total = array.reduce((acc, c) => acc + c, 0);
+    return total / array.length;
+  }
 }
